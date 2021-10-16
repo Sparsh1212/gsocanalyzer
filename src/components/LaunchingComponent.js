5949542ec26a9df14a35c4ac7cca7f2ad556fa36
Sparsh Agrawal <sparshagrawal1212@gmail.com>
2021-10-16 13:49:22 +0530

    Add full stop

--- src/components/LaunchingComponent.js
+++ src/components/LaunchingComponent.js
@@ -10,7 +10,7 @@ const LaunchingComponent = () => {
         <div id='animatedText'>
           <p>
             A blazingly fast tool to get the past 12 year analytics of
-            organizations selected in GSoC
+            organizations selected in GSoC.
           </p>
           <Typist.Delay ms={150} />
           <p>Search by tech-stack / organisation name / category or by topic.</p>
