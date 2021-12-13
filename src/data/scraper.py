115ede5b766b3bca657cc5a7f00ba684c023f3c3
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2021-12-13 20:08:45 +0530

    fix an issue

--- src/data/scraper.py
+++ src/data/scraper.py
@@ -1,4 +1,5 @@
 import json
+from typing import NewType
 import requests
 
 r = requests.get('https://api.gsocorganizations.dev/organizations.json')
@@ -13,10 +14,13 @@ for org in data:
     newData['cat'] = org['category']
     newData['top'] = org['topics']
     newData['project'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    newData['year'] = []
     for year in org['years']:
         numYear = int(year)
         newData['url'] = org['years'][year]['projects_url'] # losing url on successive iteration is intentional, to keep the link to projects from the latest year
         newData['project'][numYear - 2009] = org['years'][year]['num_projects']
+        if org['years'][year]['num_projects'] != 0:
+            newData['year'].append(numYear)
     newOrgs.append(newData)
 
 f = open('src/data/finalData.json', 'w')
