2a4860bef4b9bc3027f72eaed3fc45065328d5cd
samratmiddha <s_middha@me.iitr.ac.in>
2022-12-20 17:44:05 +0530

    revised script

--- scrape.py
+++ scrape.py
@@ -1,74 +1,109 @@
-import requests
+
 import html5lib
 from bs4 import BeautifulSoup
 import json
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
 
 base="https://summerofcode.withgoogle.com"
 data=[]
+pg=webdriver.Chrome(executable_path="/Applications/driver/chromedriver")
+for year in reversed(range(2016,2023)):
+  url="https://summerofcode.withgoogle.com/archive/"+str(year)+'/organizations'
+  print(url,'url')
+  pg.maximize_window()
+  pg.get(url)
+  sleep(5)
+  soup=BeautifulSoup(pg.page_source,'html5lib')
+  x=True
 
-for year in reversed(range(2016,2021)):
-  url="https://summerofcode.withgoogle.com/archive/"+str(year)+'/organizations/'
-  pg=requests.get(url)
-  soup=BeautifulSoup(pg.content,'html5lib')
-  org=soup.findAll('a',class_='organization-card__link')
-  print("Scraping", year)
+  org=soup.find_all('a','content')
+  while x==True:
+    next_page=pg.find_element(By.CLASS_NAME,"mat-focus-indicator.mat-tooltip-trigger.mat-paginator-navigation-next.mat-icon-button.mat-button-base")
+    if next_page.get_property('disabled'):
+      break
+    next_page.click()
+       # next_page = pg.find_element(By.XPATH, '//button[contains(@class, "mat-focus-indicator mat-tooltip-trigger mat-paginator-navigation-next mat-icon-button mat-button-base")]')
+    print(next_page.get_property('disabled'))
+    sleep(2)
+    soup=BeautifulSoup(pg.page_source,'html5lib')
+    sleep(2)
+    org2=soup.find_all('a','content')
+    org=org+org2
+  print("Scraping", year,"first loop")
+  print(len(org), 'length')
   for i in range(len(org)):
     dic={}
     urlo=base+(org[i].attrs['href'])
-    pg=requests.get(urlo)
-    sp=BeautifulSoup(pg.content,'html5lib')
-    name=sp.find('h3',class_='banner__title').text
+    print(urlo)
+    pg.get(urlo)
+    sleep(5)
+    sp=BeautifulSoup(pg.page_source,'html5lib')
+    name=sp.find('span','title').string
     f=0
     for d in data:
       if d['name']==name:
         d['year'].append(year)
-        proj=sp.findAll('h5',class_="archive-project-card__student-name")
+        proj=sp.find_all('div',"contributor__content")
         d['proj'].append(len(proj))
         dic['url']=urlo
         f=1
     if(f==0):
       dic['url']=urlo
-      dic['name']=sp.find('h3',class_='banner__title').text
-      tech = sp.findAll('li', attrs={'class': 'organization__tag organization__tag--technology'})
+      dic['name']=sp.find('span','title').string
+      tech_string = sp.find('div',"tech__content").string
+      print(tech_string)
+      tech=tech_string.split(',')
       for j in range(len(tech)):
-        tech[j]=tech[j].text.strip()
+        tech[j]=tech[j].strip()
       dic['tech']=tech
-      cat = sp.find('li', attrs={'class': 'organization__tag organization__tag--category'})
-      te=cat.text.replace('\n','')
-      te=te.replace('\t','')
-      dic['cat']=te
-      top = sp.findAll('li', attrs={'class': 'organization__tag organization__tag--topic'})
+      # cat = sp.find('li', attrs={'class': 'organization__tag organization__tag--category'})
+      # te=cat.text.replace('\n','')
+      # te=te.replace('\t','')
+      dic['cat']=''
+      topic_string = sp.find('div','topics__content').string
+      top=topic_string.split(',')
+      print(top)
       for j in range(len(top)):
-        top[j]=top[j].text
+        top[j]=top[j].strip()
       dic['top']=top
       dic['year']=[year]
-      proj=sp.findAll('h5',class_="archive-project-card__student-name")
+      proj=sp.find_all('div',"contributor__content")
       dic['proj']=[len(proj)]
       data.append(dic)
-
+print (data,'oooo')
 for year in reversed(range(2009,2016)):
   url="https://www.google-melange.com/archive/gsoc/"+str(year)
   base="https://www.google-melange.com"
-  pg=requests.get(url)
-  soup=BeautifulSoup(pg.content,'html5lib')
-  org=soup.findAll('span',class_="mdl-list__item-primary-content")
-  print("Scraping", year)
+  pg.get(url)
+  sleep(2)
+  soup=BeautifulSoup(pg.page_source,'html5lib')
+  org=soup.find_all('span',class_="mdl-list__item-primary-content")
+  print("Scraping", year,"second loop")
   for i in range(len(org)):
     link=org[i].find('a')
     urlo=base+(link.attrs['href'])
-    pg=requests.get(urlo)
-    sp=BeautifulSoup(pg.content,'html5lib')
-    name=link.text
-    f=0
-    for d in data:
-      if d['name']==name:
-        d['year'].append(year)
-        proj=sp.findAll('li',class_="mdl-list__item mdl-list__item--two-line")
-        d['proj'].append(len(proj))
+    print(urlo,"second loop testing")
+    try:
+      pg.get(urlo)
+    except:
+      print('org not found',urlo)
+    else:
+      sp=BeautifulSoup(pg.page_source,'html5lib')
+      name=link.text
+      f=0
+      for d in data:
+        if d['name']==name:
+          d['year'].append(year)
+          proj=sp.findAll('li',class_="mdl-list__item mdl-list__item--two-line")
+          d['proj'].append(len(proj))
 
 for d in data:
   d['project']=[]
-  for year in range(2009,2021):
+
+  for year in range(2009,2023):
     f=1
     for y in range(len(d['year'])):
       if year==d['year'][y]:
@@ -77,6 +112,7 @@ for d in data:
         break
     if f==1:d['project'].append(0)
   d.pop('proj') 
-
+print(data,'lololo')
 with open('data.json', 'w') as fout:
     json.dump(data , fout,indent = 6)
+    print(data)
--- src/components/OrganisationCard.js
+++ src/components/OrganisationCard.js
@@ -1,59 +1,73 @@
-import React from 'react';
-import { Card} from 'semantic-ui-react';
-import '../css/mainpagecss.css';
-import { Line } from 'react-chartjs-2';
-import TopTechTag from './TopTechTag';
-import bookmarkedIcon from '../assets/bookmarked.svg'
-import notBookmarkedIcon from '../assets/not-bookmarked.svg'
+import React from "react";
+import { Card } from "semantic-ui-react";
+import "../css/mainpagecss.css";
+import { Line } from "react-chartjs-2";
+import TopTechTag from "./TopTechTag";
+import bookmarkedIcon from "../assets/bookmarked.svg";
+import notBookmarkedIcon from "../assets/not-bookmarked.svg";
 
-const OrganisationCard = ({key, orgData, bookmarked, setBookmarked}) => {  
+const OrganisationCard = ({ key, orgData, bookmarked, setBookmarked }) => {
   const isMobile = window.innerWidth <= 750;
   const graphData = {
-    labels: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+    labels: [
+      2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
+      2021, 2022,
+    ],
     datasets: [
       {
-        label: 'No. of Projects',
+        label: "No. of Projects",
         data: [...orgData.project],
-        borderColor: '#99A3A4',
-        backgroundColor: '#F2F4F4',
-        pointBorderColor: '#17202A',
-        pointBackgroundColor: '#17202A',
+        borderColor: "#99A3A4",
+        backgroundColor: "#F2F4F4",
+        pointBorderColor: "#17202A",
+        pointBackgroundColor: "#17202A",
       },
     ],
   };
 
   return (
-    <Card id='card'>
+    <Card id="card">
       <Card.Content>
         <Card.Description>
-          <Card.Header as='h1' id='orgName'>
+          <Card.Header as="h1" id="orgName">
             {orgData.name}
           </Card.Header>
-          <h1 className='family'>
-            No. of times in GSoC:{' '}
-            <span id='selectedTimes'>{orgData.year.length}</span>{' '}
+          <h1 className="family">
+            No. of times in GSoC:{" "}
+            <span id="selectedTimes">{orgData.year.length}</span>{" "}
           </h1>
-          { !bookmarked.includes(orgData) ?
-            <img className="bookmark-icon" src={notBookmarkedIcon} onClick={() => {setBookmarked([...bookmarked, orgData]);}} title="Add Bookmark"/>
-          : 
-            <img className="bookmark-icon bookmarked-icon" src={bookmarkedIcon} onClick={() => {
-              var array = bookmarked;
-              for(var i = 0; i < array.length; i++) {
-                if(array[i].name === orgData.name) {
-                  array.splice(i, 1);
-                  break;
+          {!bookmarked.includes(orgData) ? (
+            <img
+              className="bookmark-icon"
+              src={notBookmarkedIcon}
+              onClick={() => {
+                setBookmarked([...bookmarked, orgData]);
+              }}
+              title="Add Bookmark"
+            />
+          ) : (
+            <img
+              className="bookmark-icon bookmarked-icon"
+              src={bookmarkedIcon}
+              onClick={() => {
+                var array = bookmarked;
+                for (var i = 0; i < array.length; i++) {
+                  if (array[i].name === orgData.name) {
+                    array.splice(i, 1);
+                    break;
+                  }
                 }
-              }
-              setBookmarked([...array]);
-            }} title="Delete Bookmark"/>
-          }
+                setBookmarked([...array]);
+              }}
+              title="Delete Bookmark"
+            />
+          )}
 
-
-          <h3 className='family'>
-            Category: {orgData.cat != '' ? orgData.cat : 'Others'}
+          <h3 className="family">
+            Category: {orgData.cat != "" ? orgData.cat : "Others"}
           </h3>
 
-          <h3 className='family'>
+          <h3 className="family">
             Tech Stack <br />
             <div>
               {orgData.tech.map((tech) => (
@@ -61,7 +75,7 @@ const OrganisationCard = ({key, orgData, bookmarked, setBookmarked}) => {
               ))}
             </div>
           </h3>
-          <h3 className='family'>
+          <h3 className="family">
             Topics
             <br />
             <div>
@@ -70,7 +84,7 @@ const OrganisationCard = ({key, orgData, bookmarked, setBookmarked}) => {
               ))}
             </div>
           </h3>
-          <div className='graph'>
+          <div className="graph">
             <Line
               data={graphData}
               height={isMobile ? 300 : 250}
@@ -91,7 +105,12 @@ const OrganisationCard = ({key, orgData, bookmarked, setBookmarked}) => {
           <br />
           <br />
           <form target="_blank" action={orgData.url}>
-              <input className="visitUrl" type="submit" title="Visit official page" value="Visit official page" />
+            <input
+              className="visitUrl"
+              type="submit"
+              title="Visit official page"
+              value="Visit official page"
+            />
           </form>
         </Card.Description>
       </Card.Content>
